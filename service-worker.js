67682c0dc868f6256f71c70e22b7f8c68cc17076
Matthias Ritter <development@matthias-ritter.com>
2024-12-29 00:28:58 +0100

    v.1.1.0

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,13 @@
+import { BrotliDecode } from './js/external/decode.min.js';
+
 self.addEventListener('install', event => self.skipWaiting());
 self.addEventListener('activate', event => event.waitUntil(clients.claim()));
 self.addEventListener('fetch', (event) => {
     const request = event.request;
     const url = new URL(request.url);
     const path = url.pathname;
-    if (path.endsWith('.js') || path.endsWith('.map') || path.endsWith('.css') || path.endsWith('.html') || path.endsWith('.wasm') || path.endsWith('.dat')) {
+
+    if ((!path.endsWith('.razor.js') && path.endsWith('.js')) || path.endsWith('.css') || path.endsWith('.wasm') || path.endsWith('.dat')) {
         const modifiedRequest = new Request(request.url + '.br', {
             method: request.method,
             headers: request.headers,
@@ -16,20 +19,30 @@ self.addEventListener('fetch', (event) => {
             cache: request.cache,
         });
         event.respondWith(
-            fetch(modifiedRequest).then((response) => {
+            fetch(modifiedRequest).then(async (response) => {
                 if (response.ok) {
-                    const modifiedHeaders = new Headers(response.headers);
-                    modifiedHeaders.set('Content-Encoding', 'br');
-                    return new Response(response.body, {
-                        status: response.status,
-                        statusText: response.statusText,
-                        headers: modifiedHeaders,
-                    });
+                    const originalResponseBuffer = await response.arrayBuffer();
+                    const originalResponseArray = new Int8Array(originalResponseBuffer);
+                    const decompressedResponseArray = BrotliDecode(originalResponseArray);
+
+                    let contentType = 'application/octet-stream';
+                    if (path.endsWith('.js'))
+                        contentType = 'text/javascript';
+                    if (path.endsWith('.css'))
+                        contentType = 'text/css';
+                    if (path.endsWith('.wasm'))
+                        contentType = 'application/wasm';
+
+                    return new Response(decompressedResponseArray, { headers: { 'content-type': contentType } });
                 }
+                return fetch(request);
+
+            }).catch((error) => {
                 return fetch(request);
             })
         );
-    } else {
+    }
+    else {
         event.respondWith(fetch(request));
     }
 });
